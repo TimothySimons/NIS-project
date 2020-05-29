b871f0793890523b0dd152d40ad304fb1ba32d5a
TimothySimons <smntim001@myuct.ac.za>
2020-05-29 17:37:42 +0200

    Restructure Client class
    Changed Client methods to throw exceptions rather than catch their
    own exceptions. All exceptions are caught in main (as is best
    practice).

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,10 @@
 import java.io.*;
 import java.net.*;
+import javax.net.ssl.*;
+import java.security.cert.Certificate;
+import javax.net.ServerSocketFactory;
+
+// import AsymmetricEncryption;
 
 public class Client {
 
@@ -13,82 +18,102 @@ public class Client {
  */
   public static void main(String[] args) {
     /*
-     * TODO use JCommander to read commandline options.
+     * TODO: use JCommander to read commandline options.
      * Allow user to specify whether they want to generate a public-private key
      * pair or read in this pair from a file.
+     * TODO: this main function violates so many best practices; need to clean it up.
      */
-    Socket socket = null;
-    if(args.length == 1) {
-      int portNumber = Integer.parseInt(args[0]);
-      socket = listen(portNumber);
-    } else if (args.length == 2){
-      String hostName = args[0];
-      int portNumber = Integer.parseInt(args[1]);
-      socket = connect(hostName, portNumber);
-    } else {
-      System.err.println("Usage 1: java Client <port number>");
-      System.err.println("Usage 2: java Client <host name> <port number>");
-      System.exit(1);
-    }
-
-    if (socket != null) {
-      try {
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        String userInput;
-        while ((userInput = stdIn.readLine()) != null) {
-          out.println(userInput);
-          System.out.println("remote: " + in.readLine());
-        }
-      } catch (IOException e) {
-        String hostName = socket.getInetAddress().getHostName();
-        System.err.println("Couldn't get I/O for connection to " + hostName);
+     try {
+      if(args.length == 1) {
+        int portNumber = Integer.parseInt(args[0]);
+        Socket socket = listen(portNumber);
+        Certificate cert = AsymmetricEncryption.loadCertificate("alice.cer");
+        sendCert(socket, cert);
+      } else if (args.length == 2){
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+        Socket socket = connect(hostName, portNumber);
+        Certificate cert = receiveCert(socket);
+        System.out.println(cert);
+      } else {
+        System.err.println("Usage 1: java Client <port number>");
+        System.err.println("Usage 2: java Client <host name> <port number>");
+        System.exit(1);
       }
-    } else {
-      System.exit(1);
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    } catch (SocketException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
+
 
   /**
    * Listens for a connection and returns the associated socket.
    *
    * @param portNumber local port number on which to listen for a connection
-   * @return null if the connection is unsuccessful, otherwise the socket
-   * associated with the connected client
+   * @return socket associated with the connected client
    */
-  private static Socket listen(int portNumber) {
-    Socket socket = null;
-    try {
+  private static Socket listen(int portNumber) throws IOException {
       ServerSocket serverSocket = new ServerSocket(portNumber);
-      socket = serverSocket.accept();
-    } catch (IOException e) {
-      System.out.println("Server socket cannot listen on port " + portNumber);
-    } finally {
+      Socket socket = serverSocket.accept();
       return socket;
-    }
   }
+
 
   /**
    * Attempts to connect to a client.
    *
    * @param hostName the name of the remote host
    * @param portNumber the port number on which the remote host is listening
-   * @return null if the connection is unsuccessful, otherwise the socket
-   * associated with the connected client
+   * @return socket associated with the connected client
    */
-  private static Socket connect(String hostName, int portNumber) {
-    Socket socket = null;
-    try {
-      socket = new Socket(hostName, portNumber);
-    } catch (UnknownHostException e) {
-      System.err.println("Unknown host " + hostName);
-      System.exit(1);
-    } catch (IOException e) {
-      System.err.println("Couldn't get I/O for the connection to " + hostName);
-      System.exit(1);
-    } finally {
+  private static Socket connect(String hostName, int portNumber)
+  throws UnknownHostException, IOException {
+      Socket socket = new Socket(hostName, portNumber);
       return socket;
-    }
+  }
+
+
+  private static void sendCert(Socket socket, Certificate cert)
+  throws SocketException, IOException {
+      ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+      outputStream.writeObject(cert);
+  }
+
+
+  private static Certificate receiveCert(Socket socket)
+  throws SocketException, IOException, ClassNotFoundException {
+      ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+      Certificate cert = (Certificate) inStream.readObject();
+      return cert;
   }
 }
+
+
+
+
+
+
+
+//TODO: make all methods throw the relevant exceptions and catch those exceptions
+//      in main
+
+
+// try {
+//   PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//   BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//   BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+//   String userInput;
+//   while ((userInput = stdIn.readLine()) != null) {
+//     out.println(userInput);
+//     System.out.println("remote: " + in.readLine());
+//   }
+// } catch (IOException e) {
+//   String hostName = socket.getInetAddress().getHostName();
+//   System.err.println("Couldn't get I/O for connection to " + hostName);
+// }
